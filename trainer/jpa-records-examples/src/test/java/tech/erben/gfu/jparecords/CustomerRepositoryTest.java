3e73e34cb8892e1076adc8c4f6ebe36be5096078
Alexander Erben <alex@it-erben.com>
2025-12-24 13:46:33 +0100

    fix: issue with Spring Boot 4

--- trainer/jpa-records-examples/src/test/java/tech/erben/gfu/jparecords/CustomerRepositoryTest.java
+++ trainer/jpa-records-examples/src/test/java/tech/erben/gfu/jparecords/CustomerRepositoryTest.java
@@ -3,7 +3,7 @@ package tech.erben.gfu.jparecords;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 
 import java.util.List;
 
